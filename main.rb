c4fdf7271ec7a7dca9cb08a18f0bcc9f1af20773
supersakana <zacwilliamson@icloud.com>
2022-04-20 14:17:09 -0600

    finished driver script

--- main.rb
+++ main.rb
@@ -12,3 +12,20 @@ p bst.level_order(bst.root)
 p bst.preorder(bst.root)
 p bst.postorder(bst.root)
 p bst.inorder(bst.root)
+
+bst.insert(bst.root, 5)
+bst.insert(bst.root, 1)
+bst.insert(bst.root, 3)
+bst.insert(bst.root, 4)
+
+bst.pretty_print
+p bst.balanced?(bst.root)
+
+bst.rebalance(bst.root)
+bst.pretty_print
+p bst.balanced?(bst.root)
+
+p bst.level_order(bst.root)
+p bst.preorder(bst.root)
+p bst.postorder(bst.root)
+p bst.inorder(bst.root)
